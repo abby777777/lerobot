31e3a21e85a04216be912df77a982ecb0b3d7da9
Abby <abigator7@berkeley.edu>
2025-03-04 11:55:30 -0700

    changed config naming

--- lerobot/common/robot_devices/robots/configs.py
+++ lerobot/common/robot_devices/robots/configs.py
@@ -206,7 +206,7 @@ class KochRobotConfig(ManipulatorRobotConfig):
     leader_arms: dict[str, MotorsBusConfig] = field(
         default_factory=lambda: {
             "main": DynamixelMotorsBusConfig(
-                port="/dev/ttyACM_leader",
+                port="/dev/ttyACM_kochleader",
                 motors={
                     # name: (index, model)
                     "shoulder_pan": [1, "xl330-m077"],
@@ -223,7 +223,7 @@ class KochRobotConfig(ManipulatorRobotConfig):
     follower_arms: dict[str, MotorsBusConfig] = field(
         default_factory=lambda: {
             "main": DynamixelMotorsBusConfig(
-                port= "/dev/ttyACM_follower",
+                port= "/dev/ttyACM_kochfollower",
                 motors={
                     # name: (index, model)
                     "shoulder_pan": [1, "xl430-w250"],
